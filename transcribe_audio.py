926994f89bef1485dd779048bc54bae209f2e8b2
Gonzalo Perez Centeno <GonPerezCemteno@gmail.com>
2025-02-24 19:38:38 -0300

    Add transcript file via google cloud

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,39 +1,83 @@
-import os
-import sys
-import glob
-import shutil
-from openai import OpenAI
+from google.cloud import speech
+from datetime import timedelta
 
-api_key = os.getenv("OPENAI_API_KEY")
-if not api_key:
-    raise ValueError("Please set the OPENAI_API_KEY environment variable")
 
-client = OpenAI(api_key=api_key)
+def transcribe_gcs(gcs_uri: str) -> str:
+    """Asynchronously transcribes the audio file from Cloud Storage
+    Args:
+        gcs_uri: The Google Cloud Storage path to an audio file.
+            E.g., "gs://storage-bucket/file.flac".
+    Returns:
+        The generated transcript from the audio file provided.
+    """
+    client = speech.SpeechClient()
 
-input_wildcard = sys.argv[1]
-input_files = glob.glob(input_wildcard)
-print(f"Transcribing from{input_files}")
-output_files = []
+    audio = speech.RecognitionAudio(uri=gcs_uri)
 
-for x in input_files:
-    audio_file= open(x, "rb")
-    print("Transcribing file " + x)
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1",
-        file=audio_file,
-        response_format="text",
-        language="en"
+    diarization_config = speech.SpeakerDiarizationConfig(
+        enable_speaker_diarization=True,
+        min_speaker_count=2,
+        max_speaker_count=2,
     )
-    output_file = f"{x[:-4]}.txt"
-    output_files.append(output_file)
-    with open(output_file, "a", encoding="utf-8") as f:
-        print(transcription, file=f)
-        print(f"Transcription written to: {output_file}")
 
-print(f"Outputs are {output_files}")
-concat_file = os.path.dirname(output_files[0]) + "/output_file.txt"
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100, # 48000
+        language_code="es-AR",
+        enable_word_time_offsets=True,
+        diarization_config=diarization_config,
+        audio_channel_count=2,
 
-with open(concat_file,'wb') as wfd:
-    for f in output_files:
-        with open(f,'rb') as fd:
-            shutil.copyfileobj(fd, wfd)
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio, timeout=3600)
+
+    print("Waiting for operation to complete...")
+    response = operation.result()
+
+    transcript_builder = []
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    result_part = 0
+    for result in response.results:
+        result_part += 1
+        print("Processing result: batch {}".format(result_part))
+
+        # The first alternative is the most likely one for this portion.
+        best_option = result.alternatives[0]
+
+        # transcript_builder.append(f"\nConfidence: {result.alternatives[0].confidence}")
+        part_start_time = best_option.words[0].start_time.total_seconds()
+        # speakers = set()
+
+        for word_info in best_option.words:
+            word = word_info.word
+            start_time = word_info.start_time.total_seconds()
+            if part_start_time > start_time:
+                part_start_time = start_time
+            # end_time = word_info.end_time
+            # speakers.add(word_info.speaker_tag)
+
+        # list_speakers = ", ".join([str(speaker) for speaker in speakers])
+        str_time = str(timedelta(seconds=part_start_time))
+        transcript_builder.append(f"\n{str_time}\n{best_option.transcript}")
+
+    transcript = "".join(transcript_builder)
+
+    return transcript
+
+
+def to_file(file_name: str, content: str) -> None:
+    with open(file_name, "w") as file:
+        file.write(content)
+
+    
+if __name__ == "__main__":
+    carpeta = "seminario_hamlet"
+    archivo = "seminario_hamlet_clase_04"
+    for part in range(1, 11):
+        clase = f"{archivo}_part{part:02d}"
+        gcs_file = f"gs://{carpeta}/{clase}.flac"
+        transcription_file = f"transcriptions/{carpeta}/{clase}.txt"
+        transcript = transcribe_gcs(gcs_file)
+        to_file(transcription_file, transcript)
