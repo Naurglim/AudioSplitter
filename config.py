bdec73269a0323aed765711e6224e7af4a8f8c32
Gonzalo Perez Centeno <gperezcenteno@enargas.gov.ar>
2025-02-26 11:58:02 -0300

    Add configuration file and refactor main pipeline for audio processing

---
+++ config.py
@@ -0,0 +1,6 @@
+SEGMENT_LENGTH = 15*60*1000  # 15 minutes in milliseconds
+GCP_BUCKET = 'seminario_hamlet' # Name of the GCP bucket
+GCP_PROJECT = 'transcriptor-449915'  # Name of the GCP project
+PATH_AUDIO = 'audios/'  # Path where the audio files are located
+PATH_TRANSCRIPTION = 'transcriptions/'  # Path where the transcriptions will be saved
+MAX_SEGMENTS = 18  # Maximum number of parts to process
--- main_pipeline.py
+++ main_pipeline.py
@@ -8,13 +8,9 @@ from google.cloud import speech
 from datetime import timedelta
 
 from pipeline import Pipeline
+from config import SEGMENT_LENGTH, GCP_BUCKET, GCP_PROJECT, PATH_AUDIO, PATH_TRANSCRIPTION, MAX_SEGMENTS
 
-
-SEGMENT_LENGTH = 15*60*1000  # 15 minutes in milliseconds
-GCP_BUCKET = 'seminario_hamlet' # Name of the GCP bucket
-GCP_PROJECT = 'transcriptor-449915'  # Name of the GCP project
-PATH_AUDIO = 'audios/'  # Path where the audio files are located
-PATH_TRANSCRIPTION = 'transcriptions/'  # Path where the transcriptions will be saved
+# In case the folders do not exist, create them:
 os.makedirs(PATH_AUDIO, exist_ok=True)
 os.makedirs(PATH_TRANSCRIPTION, exist_ok=True)
 os.environ.setdefault("GCLOUD_PROJECT", GCP_PROJECT)
@@ -23,32 +19,34 @@ pipeline = Pipeline()
 
 @pipeline.task()
 def extract_audio(video_filename: str) -> str:
-    audio_filename =  Path(video_filename).stem + '.flac'
-    # Extract the audio from the video file
-    AudioSegment.from_file(video_filename).export(audio_filename, format='flac')
-    # Move the audio file to the audio directory   
-    full_audio_filename = os.path.join(PATH_AUDIO, audio_filename)
-    os.rename(audio_filename, full_audio_filename)
-    print(f"Audio file saved as {audio_filename}")
+    """
+    Extract the audio from the given video file and save it as a FLAC file.
+
+    The output audio file will be saved in the specified audio directory.
+    """
+    output_audio_filename = Path(video_filename).stem + '.flac'
+    full_audio_filename = os.path.join(PATH_AUDIO, output_audio_filename)
+    # Extract the audio from the video file and export directly to the destination directory
+    AudioSegment.from_file(video_filename).export(full_audio_filename, format='flac')
+    print(f"Audio file saved as {output_audio_filename}")
     return full_audio_filename
 
 
 @pipeline.task(depends_on=extract_audio)
-def split_audio(file_path): 
-    segment_length = SEGMENT_LENGTH
+def split_audio(full_audio_filename : str, segment_length : int = SEGMENT_LENGTH) -> str:
+    """
+    Split the audio file into multiple segments of a specified length.
 
-    filename = Path(file_path)
+    Each segment is saved as a separate file in the output directory.
+    The segment length is specified in milliseconds.
+    """
+    filename = Path(full_audio_filename)
     audio_stem =  filename.stem
-    original_path = filename.with_suffix('')
-    os.makedirs(original_path, exist_ok=True)
+    output_directory = filename.with_suffix('')
+    os.makedirs(output_directory, exist_ok=True)
 
-    # Load the audio file
-    audio = AudioSegment.from_file(file_path)
-    
-    # Get the total length of the audio file
+    audio = AudioSegment.from_file(full_audio_filename)
     total_length = len(audio)
-    
-    # Calculate the number of segments needed
     num_segments = math.ceil(total_length / segment_length)
 
     # Loop through and create each segment
@@ -57,18 +55,16 @@ def split_audio(file_path):
         end_time = min((i + 1) * segment_length, total_length)  # Ensure the last segment does not exceed total length
         segment = audio[start_time:end_time]
 
-        # Generate the output file name
+        # Export the segment to a flac file
         part = i + 1
-        output_file = f"{original_path}/{audio_stem}_part{part:02d}.flac"
-        
-        # Export the segment as an m4a file
+        output_file = Path(output_directory) / f"{audio_stem}_part{part:02d}.flac"
         segment.export(output_file, format="flac") 
         print(f"Exported: {output_file}")
-    return original_path
+    return str(output_directory)
 
 
 @pipeline.task(depends_on=split_audio)
-def upload_to_bucket(source_directory):
+def upload_to_bucket(source_directory : str, workers : int = 8, bucket_name : str = GCP_BUCKET) -> str:
     """Upload every file in a directory, including all files in subdirectories.
 
     Each blob name is derived from the filename, not including the `directory`
@@ -77,15 +73,8 @@ def upload_to_bucket(source_directory):
     transfer_manager.upload_many() instead.
     """
 
-    bucket_name = GCP_BUCKET
-    workers = 8
-
     storage_client = Client()
     bucket = storage_client.bucket(bucket_name)
-
-    # Generate a list of paths (in string form) relative to the `directory`.
-    # This can be done in a single list comprehension, but is expanded into
-    # multiple lines here for clarity.
 
     # First, recursively get all files in `directory` as Path objects.
     directory_as_path_obj = Path(source_directory)
@@ -120,9 +109,104 @@ def upload_to_bucket(source_directory):
 
 
 @pipeline.task(depends_on=upload_to_bucket)
-def transcribe_from_bucket():
-    pass
+def transcribe_from_bucket(source_directory : str, bucket_name : str = GCP_BUCKET) -> str:
+    """
+    Transcribe the audio files in the specified bucket and save the transcriptions in a separate directory.
+    """
+    # get the last folder of the path
+    source_path = Path(source_directory)
+    archivo = source_path.parts[-1]
+    
+    print(f"Transcribing audio file '{archivo}' from bucket: {bucket_name}")
 
+    for part in range(1, MAX_SEGMENTS + 1):
+        text_filename = f"{archivo}_part{part:02d}.txt"
+        audio_filename = f"{archivo}_part{part:02d}.flac"
+        if not file_exists(bucket_name, audio_filename):
+            print(f"Couldn't find file {audio_filename}")
+            break
+
+        gcs_file = f"gs://{bucket_name}/{audio_filename}"
+        transcription_path = Path(PATH_TRANSCRIPTION) / bucket_name
+        transcription_path.mkdir(parents=True, exist_ok=True)
+        transcription_file = f"transcriptions/{bucket_name}/{text_filename}"
+        transcript = transcribe_gcs(gcs_file)
+        to_file(transcription_file, transcript)
+
+
+def file_exists(bucket_name: str, file_name: str) -> bool:
+    storage_client = Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    return blob.exists()
+
+
+def transcribe_gcs(gcs_uri: str) -> str:
+    """Asynchronously transcribes the audio file from Cloud Storage
+    Args:
+        gcs_uri: The Google Cloud Storage path to an audio file.
+            E.g., "gs://storage-bucket/file.flac".
+    Returns:
+        The generated transcript from the audio file provided.
+    """
+    client = speech.SpeechClient()
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    diarization_config = speech.SpeakerDiarizationConfig(
+        enable_speaker_diarization=True,
+        min_speaker_count=1,
+        max_speaker_count=10,
+    )
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000, # 44100
+        language_code="es-AR",
+        alternative_language_codes=["en-US", "en-GB"],
+        enable_word_time_offsets=True,
+        enable_automatic_punctuation=True,
+        # diarization_config=diarization_config,
+        # audio_channel_count=2,
+    )
+    operation = client.long_running_recognize(config=config, audio=audio, timeout=3600)
+
+    print("Waiting for operation to complete...")
+    response = operation.result()
+
+    transcript_builder = []
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    result_part = 0
+    for result in response.results:
+        result_part += 1
+        print("Processing result: batch {}".format(result_part))
+
+        # The first alternative is the most likely one for this portion.
+        best_option = result.alternatives[0]
+
+        # transcript_builder.append(f"\nConfidence: {result.alternatives[0].confidence}")
+        part_start_time = best_option.words[0].start_time.total_seconds()
+        # speakers = set()
+
+        for word_info in best_option.words:
+            word = word_info.word
+            start_time = word_info.start_time.total_seconds()
+            if part_start_time > start_time:
+                part_start_time = start_time
+            # end_time = word_info.end_time
+            # speakers.add(word_info.speaker_tag)
+
+        # list_speakers = ", ".join([str(speaker) for speaker in speakers])
+        str_time = str(timedelta(seconds=part_start_time))
+        transcript_builder.append(f"\n{str_time}\n{best_option.transcript}")
+
+    transcript = "".join(transcript_builder)
+
+    return transcript
+
+
+def to_file(file_name: str, content: str) -> None:
+    with open(file_name, "w") as file:
+        file.write(content)
+   
 
 if __name__ == '__main__':
     pipeline.run(video_filename=sys.argv[1])
